08d886cb0ae38f4776f8f11622edf4060ff3e696
arandomguy879 <lkearney627@gmail.com>
2024-10-06 22:50:14 +0100

    Add files via upload

---
+++ namegame.py
@@ -0,0 +1,29 @@
+import os
+os.system('cls')
+name = input("what's your name?: ")
+if name == "ciara":
+    os.system('cls')
+    print("idk")
+    input("press enter to continue")
+else:
+    if name == "lee":
+        os.system('cls')
+        print("your the best")
+        input("press enter to continue")
+    else:
+        if name == "josh":
+            os.system('cls')
+            print("your a weirdo")
+        else:
+            if name == "ella":
+                os.system('cls')
+                print("your a loser")
+                input("press enter to continue")
+            else:
+                if name == "john":
+                    os.system('cls')
+                    print("what kind of npc name is that??????")
+                else:
+                    os.system('cls')
+                    print("name is not on the list it will be later")
+                    input("press enter to continue")
